741548c0ee92e7820b477d510adc6422f3908e84
sh1ro9ma <145523087+sh1ro9ma@users.noreply.github.com>
2023-09-24 14:19:03 +0000

    hookscriptの参照先を変更させるbatファイル

---
+++ shareHookScript/chgGitConfig.bat
@@ -0,0 +1,11 @@
+@echo off
+REM ファイルをコピーするバッチファイル
+chcp 65001
+
+REM hookscriptの参照先ディレクトリを変更する
+git config --local core.hooksPath .githooks
+
+REM 変更後の状態を確認できるようにしておく
+echo changed: local core hooksPath
+git config --local core.hooksPath
+pause
