b4b75967e7a6964f0ea319ca215689a0e05d665a
sh1ro9ma <145523087+sh1ro9ma@users.noreply.github.com>
2023-09-24 10:07:05 +0000

    ファイルコピースクリプトの更新

--- shareHookScript/cpHookScript.bat
+++ shareHookScript/cpHookScript.bat
@@ -1,36 +1,47 @@
 @echo off
 REM ファイルをコピーするバッチファイル
+chcp 65001
 
 REM コピー元ファイルのパス
-set source_file=lochooks\post-update
+set src_folder=lochooks\
+set file_name=post-merge
+set src_path=%src_folder%%file_name%
 
-REM コピー先フォルダのパス
-set destination_folder=.git\hooks\
+REM コピー先ファイルのパス
+set dst_folder=.git\hooks\
+set dst_path=%dst_folder%%file_name%
+
+echo SRC:"%src_path%"
+echo DST:"%dst_path%"
+echo.
 
 REM コピー元ファイルが存在するか確認
-if not exist "%source_file%" (
-    echo "can't find copy file"
+if not exist "%src_path%" (
+    REM 処理を続行できないので終了
+    echo Source file not found
     goto fin
 )
 
-echo "src ok"
-pause
-REM コピー先ファイルが存在するか確認
-if exist "%destination_folder%\File.txt" (
-    echo コピー先ファイルが既に存在します。上書きしますか？ (Y/N)
-    set /p overwrite=
-    if /i "%overwrite%"=="Y" (
-        copy /y "%source_file%" "%destination_folder%\File.txt"
-        echo ファイルを上書きコピーしました。
-    ) else (
-        echo コピーをキャンセルしました。
-    )
-) else (
-    copy "%source_file%" "%destination_folder%\File.txt"
-    echo ファイルをコピーしました。
+REM ファイルをコピーしたら終了
+if not exist "%dst_path%" (
+    copy "%src_path%" "%dst_path%"
+
+    REM 処理を終えたので終了
+    echo File copy completed
+    goto fin
 )
 
+REM 上書きを確認する
+echo Destination file found
+echo Do you want to overwrite? (Y/N)
+set /p overwrite=
+if /i "%overwrite%"=="Y" (
+    copy /y "%src_path%" "%dst_path%"
+    echo File overwritten
+) else (
+    echo File copy canceled
+)
 
 :fin
-REM バッチファイルの実行を一時停止（Enterキーを押すまで待つ）
+REM バッチファイルの実行を一時停止（キーを押すまで待つ）
 pause
