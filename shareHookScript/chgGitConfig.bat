4f251e115f34bbde55a819f35439f25b677050c4
sh1ro9ma <145523087+sh1ro9ma@users.noreply.github.com>
2023-09-24 14:56:19 +0000

    コメント修正

--- shareHookScript/chgGitConfig.bat
+++ shareHookScript/chgGitConfig.bat
@@ -5,7 +5,7 @@ chcp 65001
 REM hookscriptの参照先ディレクトリを変更する
 git config --local core.hooksPath .githooks
 
-REM 変更後の状態を確認できるようにしておく
+REM 変更後の状態を確認する
 echo changed: local core hooksPath
 git config --local core.hooksPath
 pause
