6c09c6fe0a25ce5d7960f4414000d661a64616d5
sh1ro9ma <145523087+sh1ro9ma@users.noreply.github.com>
2023-10-02 16:10:03 +0000

    初期状態を用意

---
+++ opeGit/opeGitCmd.py
@@ -0,0 +1,30 @@
+import subprocess
+
+
+def run_cmd_command(command, repoPath):
+    try:
+        result = subprocess.run(command, cwd=repoPath, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True, check=True)
+        if result.returncode == 0:
+            return result.stdout
+        else:
+            print("Error:", result.stderr)
+            return None
+    except Exception as e:
+        print("Error:", str(e))
+        return None
+
+
+# プッシュ対象のブランチ名
+branch_name = "master"
+
+# Gitリポジトリのディレクトリパス
+repo_directory = r"D:\work\gitPushTest\temporary"
+
+command = "git pull"
+run_cmd_command(command, repo_directory)
+
+# # 最新の変更を取得（git pull）
+# subprocess.run(["git", "pull"], cwd=repo_directory, )
+
+# # プッシュを実行
+# subprocess.run(["git", "push", "origin", branch_name], cwd=repo_directory, check=True)
