3cdf52be3211633ea3397929f8e061471e0bfce7
sh1ro9ma <145523087+sh1ro9ma@users.noreply.github.com>
2023-11-01 15:39:00 +0000

    正攻法で空コメントを検知する

--- svnHooks/pre-commit.bat
+++ svnHooks/pre-commit.bat
@@ -6,29 +6,13 @@ set TXN=%2
 
 REM 取得した情報からコメント取得してファイル出力する
 REM C:\Users\(USERNAME)\AppData\Local\Temp
-svnlook log -t "%TXN%" "%REPOS%" > tmpSVNcmnt.txt
-
-REM 出力したファイルに関する情報をさらにファイル出力
-dir | FindStr "tmpSVNcmnt.txt" > sizSVNcmnt.txt
-
-REM ファイルに関する情報を取得
-set /p FindLine=<sizSVNcmnt.txt
-
-REM 取得の終わったファイルを削除しておく
-del tmpSVNcmnt.txt
-del sizSVNcmnt.txt
-
-REM ファイルサイズに相当する文字を取得する
-REM 下記フォーマットを想定し、空白で区切った3番目の要素取得(下記なら0)
-REM 2023/10/26  22:43                 0 test.txt
-for /f "tokens=3 delims= " %%a in ("%FindLine%") do (
-set /a SIZE=%%a
+for /f "usebackq delims=" %%a in (`svnlook log -t "%TXN%" "%REPOS%"`) do (
+set COMMENT=%%a
 )
 
 REM 一文字も入力されていなければ中断
-REM ファイル出力している都合上改行文字の2byteが下限となっている
-if %SIZE% leq 2 (
-echo "Please input comment" >&2
+if "%COMMENT%" =="" (
+echo "コメントを1文字以上入力してください。" >&2
 exit 1
 )
 
