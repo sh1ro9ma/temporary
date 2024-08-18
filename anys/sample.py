94a6648b32c81d0e2a9972c218d71640069dee17
sh1ro9ma <145523087+sh1ro9ma@users.noreply.github.com>
2024-08-18 19:31:10 +0900

    Update sample.py

--- anys/sample.py
+++ anys/sample.py
@@ -1,60 +1,113 @@
 import base64
+import glob
+import os
+import sys
 import urllib.parse
 
 
-def sample_encode(tgt: str) -> str:
+def url_encode(tgt: str) -> str:
     quote = urllib.parse.quote(tgt)
-    print(f"Tgt: {tgt},\t Enc: {quote}")
 
     return quote
 
 
-def sample_decode(tgt: str) -> str:
-    unquote = urllib.parse.unquote(tgt)
-    print(f"Tgt: {tgt},\t Dec: {unquote}")
-
-    return unquote
-
-
-def sample_bytes2base64(bytes: bytes) -> bytes:
+def convert_bytes2base64(bytes: bytes) -> bytes:
     bytes_base64 = base64.b64encode(bytes)
-    print(bytes_base64)
 
     return bytes_base64
 
 
-def sample_img2str(img_path: str) -> str:
+def convert_img2str(img_path: str) -> str:
     with open(img_path, "rb") as f:
         img_bytes = f.read()
 
-    bytes_base64 = sample_bytes2base64(img_bytes)
-    str_base65 = bytes_base64.decode()
-    return str_base65
+    bytes_base64 = convert_bytes2base64(img_bytes)
+    str_base64 = bytes_base64.decode()
+    return str_base64
+
+
+class ArgInfo:
+    html_file_path_m: str = ""
+    img_folder_path_m: str = ""
+
+    def __init__(self) -> None:
+        pass
+
+    def check_argv(self) -> bool:
+        if len(sys.argv) < 3:
+            print("引数で入力ファイルを指定してください。")
+            print("引数1: 変換したいhtmlファイル")
+            print("引数2: 画像ファイルの配置されたフォルダパス")
+            return False
+
+        html_file_path = sys.argv[1]
+        if os.path.isfile(html_file_path) is False:
+            print(f"存在するファイルを指定してください。\r\n現在の指定パス: {html_file_path}")
+            return False
+
+        if html_file_path.endswith(".html") is False:
+            print(f"htmlファイルを指定してください。\r\n現在の指定パス: {html_file_path}")
+            return False
+
+        img_folder_path = sys.argv[2]
+        if os.path.isdir(img_folder_path) is False:
+            print(f"存在するフォルダを指定してください。\r\n現在の指定パス: {img_folder_path}")
+            return False
+
+        img_file_path_list = glob.glob(f"{img_folder_path}\\**\\*.*", recursive=True)
+        if len(img_file_path_list) == 0:
+            print(f"画像ファイルが存在しません。\r\n現在の指定パス: {img_folder_path}")
+            return False
+
+        self.html_file_path_m = html_file_path
+        self.img_folder_path_m = img_folder_path
+        self.img_file_path_list_m = img_file_path_list
+        return True
+
+    def output_file_name(self) -> str:
+        file_path, file_name = os.path.split(self.html_file_path_m)
+        name, ext = file_name.split(".")
+        return f"{file_path}\\{name}_rep.{ext}"
+
+    def convert_image_info_list(self) -> list[list[str]]:
+        image_info_list = []
+        for img_file_path in self.img_file_path_list_m:
+            # 画像のファイル名をパーセントエンコードする
+            img_file_name_encoded_url = url_encode(os.path.basename(img_file_path))
+            tgt_pattern = f'/{img_file_name_encoded_url}" alt="'
+
+            # 画像をbase64形式に変換して埋め込む
+            img_file_converted_str = convert_img2str(img_file_path)
+            rep_str = f'<p><img src="data:image/png;base64,{img_file_converted_str}" /></p>'
+            image_info_list.append([tgt_pattern, rep_str])
+
+        return image_info_list
 
 
 def main() -> None:
-    str_quote = sample_encode("画像テスト.png")
-    sample_decode(r"%E7%94%BB%E5%83%8F%E3%83%86%E3%82%B9%E3%83%88.png")
-    bytes_img = sample_img2str(r"D:\\work\\Otr\\02aboutGit\\img\\画像テスト.png")
+    # 引数チェック
+    arg_info = ArgInfo()
+    if arg_info.check_argv() is False:
+        return
 
-    file_name = "aboutGit.html"
-    base_path = r"D:\\work\\Otr\\02aboutGit\\"
-    tgt_html = base_path + file_name
-    with open(tgt_html, encoding="utf-8") as f:
+    # htmlファイルの読み込み
+    with open(arg_info.html_file_path_m, encoding="utf-8") as f:
         tgt_html_lines = f.readlines()
 
-    tgt_str = f'<p><img src="./img/{str_quote}" alt="test"></p>'
-    rep_str = f'<p><img src="data:image/png;base64,{bytes_img}" /></p>'
-
+    # htmlファイル内の画像をURIからbase64に置換する
+    img_info_list = arg_info.convert_image_info_list()
     for cnt, tgt_html_line in enumerate(tgt_html_lines):
-        if tgt_str in tgt_html_line:
-            print(tgt_html_line)
-            tgt_html_lines[cnt] = rep_str
+        for img_info in img_info_list:
+            if img_info[0] in tgt_html_line:
+                tgt_html_lines[cnt] = img_info[1]
+                break
 
-    rep_html = base_path + "rep_" + file_name
-    with open(rep_html, "w", encoding="utf-8") as f:
+    # 画像を埋め込んだhtmlファイルの保存
+    with open(arg_info.output_file_name(), "w", encoding="utf-8") as f:
         f.writelines(tgt_html_lines)
 
 
 if __name__ == "__main__":
+    sys.argv.append(r"path\to\html\file\xxx.html")
+    sys.argv.append(r"path\to\img\folder")
     main()
